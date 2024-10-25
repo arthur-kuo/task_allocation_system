66625b61d7a0d2e63511bf0394c5e48f2856b598
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-10-25 14:11:27 +0800

    feat: remove docker compose version

--- task_management/views.py
+++ task_management/views.py
@@ -82,6 +82,7 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def delete(self, request, id = None):
         task = self.get_object()
+        print(task)
         if task.client != request.user:
             return Response({'detail': 'You do not have permission to cancel this task.'}, status=status.HTTP_403_FORBIDDEN)
         elif task.is_finished:
